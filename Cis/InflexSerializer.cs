1b4fd18df5273e7fca8555ab050cb08c5a6c85d3
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-04-20 09:11:12 -0700

    first push

---
+++ Cis/InflexSerializer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace CustomInflexSerializer
+{
+    public class InflexSerializer : IFormatter
+    {
+        private readonly Type _type;
+        public InflexSerializer(Type type) => _type = type;
+
+        public object Deserialize(Stream serializationStream)
+        {
+            object obj = Activator.CreateInstance(_type);
+            using (StreamReader reader = new StreamReader(serializationStream))
+            { 
+                reader.ReadLine();
+                string contents = reader.ReadToEnd();
+                List<string> pairs = contents.Split(new[] {"\n", "\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                foreach (string pair in pairs)
+                {
+                    string[] keyValue = pair.Split(':');
+
+                    string key = keyValue.First();
+                    string value = keyValue.Last();
+                    
+                    PropertyInfo propertyInfo = _type.GetProperty(key);
+                    
+                    if (propertyInfo != null) break;
+                    TypeConverter typeConverter = TypeDescriptor.GetConverter(propertyInfo.PropertyType);
+                    
+                    propertyInfo.SetValue(obj, typeConverter.ConvertFromString(value), null);
+                    
+                }
+            }
+            return obj;
+        }
+
+        public void Serialize(Stream serializationStream, object graph)
+        {
+            List<PropertyInfo> properties = _type.GetProperties().ToList();
+            StreamWriter streamWriter = new StreamWriter(serializationStream);
+            streamWriter.WriteLine($"[{_type.Name}]");
+            foreach (PropertyInfo propertyInfo in properties)
+            {
+                streamWriter.WriteLine(string.Format($"{propertyInfo.Name}:{propertyInfo.GetValue(graph)}"));
+            }
+            
+            streamWriter.Flush();
+        }
+
+        public SerializationBinder Binder { get; set; }
+        public StreamingContext Context { get; set; }
+        public ISurrogateSelector SurrogateSelector { get; set; }
+    }
+}
---
+++ Cis/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Cis
+{
+    using CustomInflexSerializer;
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Test test = new Test(1, "one", true);
+            CustomSerializer(typeof(Test), test, "Test.cis");
+            Test postTest = CustomDeserialize(typeof(Test), "Test.cis") as Test;
+            Console.WriteLine($"{postTest.Number}, {postTest.Word}, {postTest.Boolean}");
+        }
+
+        private static void CustomSerializer(Type type, object data, string path)
+        {
+            InflexSerializer inflexSerializer = new InflexSerializer(type);
+            if (File.Exists(path))File.Delete(path);
+            FileStream fileStream = File.Create(path);
+            inflexSerializer.Serialize(fileStream, data);
+            fileStream.Close();
+        }
+        private static object CustomDeserialize(Type type, string path)
+        {
+            object obj = null;
+            InflexSerializer inflexSerializer = new InflexSerializer(type);
+            if (File.Exists(path))
+            {
+                FileStream fileStream = File.OpenRead(path);
+                obj = inflexSerializer.Deserialize(fileStream);
+                fileStream.Close();
+            }
+
+            return obj;
+        }
+    }
+
+    public class Test
+    {
+        public Test() { }
+
+        public Test(int number, string word, bool boolean)
+        {
+            Number = number;
+            Word = word;
+            Boolean = boolean;
+        }
+
+        public int Number { get; set; }
+        public string Word { get; set; }
+        public bool Boolean { get; set; }
+    }
+}
