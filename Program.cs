1964f7b893fb232e3dba9936f70cc57bf9532433
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-04-20 15:08:57 -0700

    added SerializeObject and merged methods into one class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text.Json.Serialization;
 
 namespace Cis
 {
@@ -10,32 +9,13 @@ namespace Cis
         private static void Main()
         {
             Test test = new Test(1, "one", true);
-            CustomSerializer(typeof(Test), test, "Test.cis");
-            Test postTest = CustomDeserialize(typeof(Test), "Test.cis") as Test;
+
+            string rron = RronConvert.SerializeObject<Test>(test);
+            File.WriteAllText("Test.rron", rron);
+
+            RronConvert.SerializeToFile<Test>(test, "Test.cis");
+            Test postTest = (Test) RronConvert.DeserializeFromFile<Test>("Test.cis");
             Console.WriteLine($"{postTest.Number}, {postTest.Word}, {postTest.Boolean}");
-        }
-
-        private static void CustomSerializer(Type type, object data, string path)
-        {
-            InflexSerializer<Test> inflexSerializer = new InflexSerializer<Test>();
-            if (File.Exists(path))File.Delete(path);
-            FileStream fileStream = File.Create(path);
-            inflexSerializer.Serialize(fileStream, data);
-            fileStream.Close();
-        }
-        
-        private static object CustomDeserialize(Type type, string path)
-        {
-            object obj = null;
-            InflexSerializer<Test> inflexSerializer = new InflexSerializer<Test>();
-            if (File.Exists(path))
-            {
-                FileStream fileStream = File.OpenRead(path);
-                obj = inflexSerializer.Deserialize(fileStream);
-                fileStream.Close();
-            }
-
-            return obj;
         }
     }
 }
